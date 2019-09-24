c3c0f888024fa22dad5a2160be3cd3ed3ca655f5
jayabalandevops <balaniaf@gmail.com>
2019-09-24 11:25:17 +0530

    added

--- cpl.groovy
+++ cpl.groovy
@@ -1,10 +1,10 @@
-pipelineJob('P1DSJ') {
+pipelineJob('PipelineDSJ-Home-1') {
     parameters {
 		stringParam('ApplicationRepository',"$ApplicationRepo", "Application code repo to build")
     }
     definition {
         cps {
-			def jobScript = readFileFromWorkspace('cit.groovy')
+			def jobScript = readFileFromWorkspace('cithome.groovy')
 			script(jobScript)
 			def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
 			approvals.approveScript(approvals.hash(jobScript,"groovy"))
