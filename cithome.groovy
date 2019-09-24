3f5aa187b19af0491cede79e827c6e5dba866167
jayabalandevops <balaniaf@gmail.com>
2019-09-24 09:27:44 +0530

    added two files

---
+++ cithome.groovy
@@ -0,0 +1,21 @@
+node('master') {
+    git(
+			url: "$ApplicationRepository",
+			branch: "master"
+		)
+    stage("Prepare"){
+        println("Preparing...")
+    }
+    stage("Clone"){
+        load 'app/clne.groovy'
+    }
+    stage("Build"){
+        println("Building the app using maven")
+    }
+    stage("Test"){
+        println("Testing...")
+    }
+    stage("Deploy"){
+        println("Deploying...")
+    }
+}
